6ebad5988f57d8fe19b7403a78db2285afc1c665
Joel Jacob Stephen <70131076+JoelJacobStephen@users.noreply.github.com>
2024-12-19 19:05:09 -0600

    style: updated styling for file upload component

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -13,14 +13,14 @@ const FileUpload = () => {
   });
 
   return (
-    <div className="h-2 bg-white rounded-xl">
+    <div className="p-2 bg-white rounded-xl">
       <div
         {...getRootProps({
           className:
-            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8",
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
-        <input {...getInputProps} />
+        <input {...getInputProps()} />
         <>
           <Inbox className="w-10 h-10 text-blue-500" />
           <p className="mt-2 text-sm test-slate-400">Drop PDF Here</p>
